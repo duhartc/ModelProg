eb0b1005d17353cb75d7238d7f0e2cfa3bddb32f
Ruimy Benjamin Mac <ruimyb@gmail.com>
2016-03-24 11:29:24 +0100

    Good CMakeList

---
+++ TP4_duhartc_ruimyb/src/Triangle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ruimy Benjamin Mac on 24/03/2016.
+//
+
+#include "Triangle.h"
---
+++ TP4_duhartc_ruimyb/src/Triangle.h
@@ -0,0 +1,14 @@
+//
+// Created by Ruimy Benjamin Mac on 24/03/2016.
+//
+
+#ifndef TP4_DUHARTC_RUIMYB_TRIANGLE_H
+#define TP4_DUHARTC_RUIMYB_TRIANGLE_H
+
+
+class Triangle {
+
+};
+
+
+#endif //TP4_DUHARTC_RUIMYB_TRIANGLE_H
